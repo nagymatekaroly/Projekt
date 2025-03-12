0ba322d9ab0c31e498d6894442d1468720153bee
nagymatekaroly <nagymatekaroly@gmail.com>
2025-03-12 15:03:36 +0100

    Before Tile Tearing

---
+++ Assets/Finish_Point.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Finish_Point : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Hero"));
+    }
+
+}
