5e8d044e786502d414608fa3868b943051bc941c
nagymatekaroly <nagymatekaroly@gmail.com>
2025-03-14 10:20:11 +0100

    Traps, Canvas bug fixed Level 2-3 parts of the level destroyed

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    GameController gameController;
+    public Transform respawnPoint;
+    private void Awake()
+    {
+        gameController = GameObject.FindGameObjectWithTag("Player").GetComponent<GameController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) 
+        {
+            gameController.UpdateCheckpoint(respawnPoint.position);
+        }
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    Vector3 checkpointPos;
+    Rigidbody2D playerRb;
+    Vector3 originalScale; // T�roljuk az eredeti m�retet
+
+    private void Awake()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        originalScale = transform.localScale; // Eredeti m�ret ment�se
+    }
+
+    private void Start()
+    {
+        checkpointPos = transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Obstacle"))
+        {
+            Die();
+            Debug.Log("Collided with: " + collision.gameObject.name);
+        }
+    }
+
+   
+
+    public void UpdateCheckpoint(Vector3 pos) 
+    {
+        checkpointPos = pos;
+    }
+    void Die()
+    {
+        StartCoroutine(Respawn(0.5f));
+    }
+    IEnumerator Respawn(float duration)
+    {
+        playerRb.simulated = false;
+        playerRb.linearVelocity = Vector3.zero;
+        transform.localScale = Vector3.zero; // Null�ra cs�kkentj�k a m�retet
+        yield return new WaitForSeconds(duration);
+        transform.position = checkpointPos;
+        transform.localScale = originalScale; // **Az eredeti m�retet �ll�tjuk vissza!**
+        playerRb.simulated = true;
+    }
+}
